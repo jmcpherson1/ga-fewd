f8b8774f786dae98f603ef709d6c138cfc172476
jmcpherson1 <jamasa.mcpherson@gmail.com>
2017-03-29 17:10:40 -0400

    Initial Commit - Dropdown Buttons
    Flexbox and jQuery toggle();

---
+++ d12/materials/dropdown-buttons/js/project.js
@@ -0,0 +1,20 @@
+
+$("#btnFile").on("click", function () {
+   $("#file").toggle();
+});
+
+$("#btnEdit").on("click", function () {
+   $("#edit").toggle();
+});
+
+/*
+$("button").on("click", function () {
+   $("ul").toggleClass("hide");
+});
+*/
+
+/*
+$("#btnFile").on("click", function () {
+   $("#file").css("display", "block");
+});
+*/
