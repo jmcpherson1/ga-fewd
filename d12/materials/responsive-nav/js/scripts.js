bcfe608312c5173063b363aea58cd22b1d3307b9
jmcpherson1 <jamasa.mcpherson@gmail.com>
2017-04-03 22:57:52 -0400

    Modal and Responsive Nav
    Modal: added jQuery overlay. Need to figure out how to turn off
    “focusout” function on “.modal” click
    
    Responsive Nav: add function to detect window size and override nav css
    display:none. Need to figure out “focusout” trigger function

--- d12/materials/responsive-nav/js/scripts.js
+++ d12/materials/responsive-nav/js/scripts.js
@@ -1,9 +1,28 @@
+//When the button is clicked
+//Add toggle the nav
+//When anywhere on page is clicked, close nav
+//When page is more than 767px, show Nav (display:block)
+   //If the page width is more than 767px display:block on nav
+
 
 $("#menu-icon").on("click", function () {
    $("nav").toggle();
 });
 
-// $("#menu-icon").on("focusout", function () {
-//    $("nav").hide(); 
-// });
+//Can't figure out why this isn't working
+$("#menu-icon").on("focusout", function () {
+   $("nav").hide();
+});
+
+//Override display:none on nav to show
+$(window).resize(function() {
+   var window = $("html").width();
+   if (window > 767) {
+      $("nav").show();
+   } else if (window < 768) {
+      $("nav").hide();
+   }
+});
+
+
 
--- d13/modal/js/project.js
+++ d13/modal/js/project.js
@@ -1,11 +1,15 @@
+//When button is clicked
+//Toggle modal on and off
+//Fade in overlay
+//When outside of modal is clicked, hide modal and overlay
+
 $("a").on("click", function () {
    $(".modal").toggleClass("show-modal");
+   $(".overlay").fadeIn();
 });
 
-// $("a").on("focusout", function () {
-//    $(".modal").hide();
-//    $(".modal").remove("display","none"); //trying to remove css
-// });
+$("a").on("focusout", function () {
+   $(".overlay").hide();
+   $(".modal").toggleClass("show-modal");
 
-
-
+});
