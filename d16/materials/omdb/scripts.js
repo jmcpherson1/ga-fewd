080af3254d85b2c5a131ebec10c7381a193a67d0
jmcpherson1 <jamasa.mcpherson@gmail.com>
2017-04-18 00:07:35 -0400

    Project: Markup & Sass
    Markup
    Sass setup
    Primary Button mixin
    Defined primary brand color

--- d16/materials/omdb/scripts.js
+++ d16/materials/omdb/scripts.js
@@ -1,34 +1,34 @@
-/*
-$.getJSON("url here", function(data) {
-  console.log(data);
-});
-*/
+var movies = [];
 
-
-
-$("button").on("click", function() {
+$("button.search").on("click", function() {
   var title = $("input").val();
 
   $.getJSON("http://omdbapi.com/?t=" + title, function(data) {
-  console.log(data);
-    $(".title").text(data["Title"]);
+    var titleAndRating = data["Title"] + " (" + data["Rated"] + ")"; //Create var to add rating beside title
+    $(".title").text(titleAndRating);
     $(".year").text(data["Year"]);
     $(".poster").attr("src",data["Poster"]);
     $(".plot").text(data["Plot"]);
     //Make title a list item
-    var searchHistory = $("<li>" + title + "</li>");
+    var li = $("<li>" + data["Title"] + "</li>");
     //Append title to end of page
-    $(".search-history").append(searchHistory);
+    $("ul.search-history").append(li);
+
+    var stashedMovie = {
+      title: data["Title"],
+      score: data["Ratings"][1]["Value"]
+    }
+
+    movies.push(stashedMovie);
   });
 });
 
-//When you click on list item it links to that movie page
-$(".search-history").on("click", function(event) {
-  var title = $(event.target).text();
+//When you click on the "li" it links to that movie page
+$("ul").on("click", function(event) {
+  var titleAndRating = data["Title"] + " (" + data["Rated"] + ")"; //Create var to add rating beside title
   
   $.getJSON("http://omdbapi.com/?t=" + title, function(data) {
-  console.log(data);
-    $(".title").text(data["Title"]);
+    $(".title").text(titleAndRating);
     $(".year").text(data["Year"]);
     $(".poster").attr("src",data["Poster"]);
     $(".plot").text(data["Plot"]);
@@ -36,4 +36,65 @@ $(".search-history").on("click", function(event) {
 });
 
 
+$("button.filter").on("click", function() {
+  $("ul.search-history").children().remove();
 
+/* THIS IT THE LONG WAY. USE FOR.EACH FUNCTION...
+var index = 0;
+while (index < movies.length) {
+  var scoreAsInt = parseInt(movies[index]["score"]);
+  if (scoreAsInt > 50 ) {
+    var li = $("<li>" + movies[index]["title"] + "</li>");
+    $("ul.search-history").append(li);
+  }
+  index = index + 1;
+}
+*/
+
+  movies.forEach(function(aSingleMovie) {
+    var scoreAsInt = parseInt(aSingleMovie["score"])
+    if ( scoreAsInt > 50 ) {
+      var li = $("<li>" + aSingleMovie["title"] + "</li>");
+      $("ul.search-history").append(li);
+    }
+  });
+  // get rid of all the bad movies
+  //   * search through the array
+  //   * for each movie that meets our criteria
+  //   * insert it into the DOM
+});
+
+$("button.show-all").on("click", function() {
+  $("ul.search-history").children().remove();
+
+  movies.forEach(function(aSingleMovie) {
+    var li = $("<li>" + aSingleMovie["title"] + "</li>");
+    $("ul.search-history").append(li);
+  });
+});
+
+//Find out if the checkbox is check
+//If checked, only show good movies
+//If uncheck, show all movies
+$("input[type='checkbox']").change(function() {
+  var isChecked = $("input[type='checkbox']").is(":checked");
+
+  if (isChecked === true) {
+    $("ul.search-history").children().remove();
+
+    movies.forEach(function(aSingleMovie) {
+      var scoreAsInt = parseInt(aSingleMovie["score"])
+      if ( scoreAsInt > 50 ) {
+        var li = $("<li>" + aSingleMovie["title"] + "</li>");
+        $("ul.search-history").append(li);
+      }
+    });
+  } else if (isChecked === false) {
+    $("ul.search-history").children().remove();
+
+    movies.forEach(function(aSingleMovie) {
+      var li = $("<li>" + aSingleMovie["title"] + "</li>");
+      $("ul.search-history").append(li);
+    });
+  } 
+});
